377be19bc5ca70e0b0d9d69cf5acafe7ce4ff3c6
vbevilacqua-edu <vbevila@asu.edu>
2023-02-26 18:35:31 -0700

    adding file to test git knowledge

---
+++ Project.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 26 15:43:28 2023
+
+@author: vince
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sympy as sp
+from tqdm import *
+
+M = 1
+m = 1
+d = 1
+Ip = 1
+Ix = 1
+Iy = 1
+Iz = 1
+
